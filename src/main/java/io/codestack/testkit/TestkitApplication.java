d1afd5966a3bab19284792a12aaac355fb7dc352
ChrisLing <1209430050@qq.com>
2018-12-27 11:01:46 +0800

    Initial commit

---
+++ src/main/java/io/codestack/testkit/TestkitApplication.java
@@ -0,0 +1,14 @@
+package io.codestack.testkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestkitApplication.class, args);
+    }
+
+}
+
